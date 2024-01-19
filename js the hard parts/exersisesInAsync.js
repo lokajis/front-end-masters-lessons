cb9f26eadcc288dc1fdfde91103203c211be3b6e
lokajis <alexandros.economide@gmail.com>
2024-01-19 17:18:13 +0100

    solving exercises for async part

---
+++ js the hard parts/exersisesInAsync.js
@@ -0,0 +1,41 @@
+
+function limitedRepeat() {
+    // ADD CODE HERE
+    let i = 0;
+    
+    function hiing () {
+      i++;
+      console.log("hi for now",i)
+      if (i>4){ clearInterval(myInterval);}  
+
+    }
+    
+ const myInterval = setInterval(hiing, 1000);
+  
+
+ 
+
+  }
+  // Uncomment the following line to check your work!
+//   limitedRepeat(); // should log (every second, for 5 seconds): hi for now
+  
+
+/* CHALLENGE 6 */
+
+function everyXsecsForYsecs(func, interval, duration) {
+    let currentDuration = 0;
+    let id = setInterval(() => {
+        func()
+      currentDuration += interval
+      if(currentDuration >= duration) {
+        clearInterval(id)
+      }
+    }, interval)
+  }
+  // Uncomment the following lines to check your work!
+  function theEnd() {
+      console.log('This is the end!');
+  }
+  everyXsecsForYsecs(theEnd, 2000, 20000); // should invoke theEnd function every 2 seconds, for 20 seconds): This is the end!
+  
+  
